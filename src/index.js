ea5d779bf4a98f7dab7ce2d1adc73c2f3eec03de
Johannes Schickling <schickling.j@gmail.com>
2016-08-06 11:16:02 +0200

    upgraded to use graphql-config-parser

--- src/index.js
+++ src/index.js
@@ -1,79 +1,32 @@
-import { resolve } from 'path'
 import getBabelRelayPlugin from 'babel-relay-plugin'
-import { graphql } from 'graphql'
-import { introspectionQuery } from 'graphql/utilities'
+import { parse, resolveSchema } from 'graphql-config-parser'
 import { loopWhile } from 'deasync'
-import request from 'sync-request'
 
-const packageJson = require(`${process.cwd()}/package.json`)
-const config = packageJson['react-relay-schema']
+const config = parse()
 
-if (!config) {
-  throw new Error('\nNo relay schema configuration found in package.json.\nPlease provide a schema endpoint for the key "react-relay-schema"')
+let wait = true
+let schema, error
+
+resolveSchema(config)
+  .then((result) => {
+    schema = result
+    wait = false
+  })
+  .catch((err) => {
+    error = err
+    wait = false
+  })
+
+// TODO find a cleaner way to do this
+loopWhile(() => wait)
+
+if (error) {
+  throw error
 }
-
-let source
-if (typeof config === 'object') {
-  source = process.env[config.env]
-} else {
-  source = config
-}
-
-let sourceType
-if (source.startsWith('http')) {
-  sourceType = 'url'
-} else if (source.endsWith('json')) {
-  sourceType = 'json'
-} else if (source.endsWith('js')) {
-  sourceType = 'schema'
-} else {
-  throw new Error('Invalid relay schema source')
-}
-
-let schemaData
-switch (sourceType) {
-
-  case 'schema':
-    const schemaSource = require(resolve(source))
-    let wait = true
-
-    graphql(schemaSource, introspectionQuery)
-      .then((result) => {
-        schemaData = result.data
-        wait = false
-      })
-
-    // TODO find a cleaner way to do this
-    loopWhile(() => wait)
-
-    break
-
-  case 'json':
-    const schema = require(resolve(source))
-    schemaData = schema.data
-
-    break
-
-  case 'url':
-    const res = request('GET', source)
-    if (res.statusCode !== 200) {
-      throw new Error(`Couldn't fetch schema from ${source}`)
-    }
-
-    const result = JSON.parse(res.getBody())
-    schemaData = result.data
-
-    break
-
-  default:
-    throw new Error('Invalid method. Valid keys are `schema, json, url`')
-}
-
-console.log(`\nRelay schema successfully loaded from ${source}`)
 
 export default function (babel) {
-  if (schemaData) {
-    return getBabelRelayPlugin(schemaData)(babel)
+  if (schema.data) {
+    return getBabelRelayPlugin(schema.data)(babel)
   } else {
     return {
       visitor: {}
