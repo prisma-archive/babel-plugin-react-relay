4e68bcb1051efd1a36719435c02d4f8e8d42835f
cahnory <cahnory@gmail.com>
2016-09-28 12:28:18 +0200

    Fix issue #6

--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ import { loopWhile } from 'deasync'
 
 const config = parse()
 
-let wait = true
-let schema, error
-
-resolveSchema(config)
-  .then((result) => {
-    schema = result
-    wait = false
-  })
-  .catch((err) => {
-    error = err
-    wait = false
-  })
-
-// TODO find a cleaner way to do this
-loopWhile(() => wait)
-
-if (error) {
-  throw error
-}
-
 export default function (babel) {
+  let wait = true
+  let schema, error
+
+  resolveSchema(config)
+    .then((result) => {
+      schema = result
+      wait = false
+    })
+    .catch((err) => {
+      error = err
+      wait = false
+    })
+
+  // TODO find a cleaner way to do this
+  loopWhile(() => wait)
+
+  if (error) {
+    throw error
+  }
+
   if (schema.data) {
     return getBabelRelayPlugin(schema.data)(babel)
   } else {
